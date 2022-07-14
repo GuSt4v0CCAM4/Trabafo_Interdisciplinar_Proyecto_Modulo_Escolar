450df7868af10dc9dabc7b7f2ae7dab64c29f02b
Gustavo Ccama Marron <104206033+GuSt4v0CCAM4@users.noreply.github.com>
2022-07-14 13:26:57 -0500

    Update connect.php

--- index/EPCC/include/connect.php
+++ index/EPCC/include/connect.php
@@ -1,3 +1,4 @@
 <?php
-	$connect= mysqli_connect('localhost','root','','sge_tusolutionweb') or die('Error en la conexion de servidor');
+//php que realiza la conexion a la base de datos
+	$connect= mysqli_connect('localhost','root','','modulo_escolar') or die('Error en la conexion de servidor');
 ?>
